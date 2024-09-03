a384c725bede56edb4e29bb476f55b4b8030e8c6
Shivam S. <shivamsahu258@outlook.com>
2024-09-03 06:22:15 +0530

    uploaded

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require("cors")
+const axios = require('axios');
+const app = express();
+
+
+app.use(cors({
+    origin: "*"
+}))
+
+
+app.get("/isrunning", (req, res) => {
+    res.send("yes this is running!")
+})
+
+app.get('/fetch-telegram-chat', async (req, res) => {
+
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const targetUrl = 'https://t.me/rec_coders/13';
+    try {
+        const response = await axios.get(proxyUrl + targetUrl, {
+            headers: {
+                "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Mobile Safari/537.36",
+                "Cookie": "stel_web_auth=https%3A%2F%2Fweb.telegram.org%2Fa%2F; stel_on=1; stel_dt=-330; stel_ssid=a7b8a0e2201748fe06_14884534864203537201",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+                "Accept-Encoding": "gzip, deflate, br",
+                "Accept-Language": "en-US,en;q=0.9,hi;q=0.8",
+                "sec-ch-ua": "\"Chromium\";v=\"128\", \"Not;A=Brand\";v=\"24\", \"Google Chrome\";v=\"128\"",
+                "sec-ch-ua-mobile": "?1",
+                "sec-ch-ua-platform": "\"Android\"",
+                "sec-fetch-dest": "document",
+                "sec-fetch-mode": "navigate",
+                "sec-fetch-site": "none",
+                "sec-fetch-user": "?1",
+                "upgrade-insecure-requests": "1"
+            }
+        });
+        res.send(response.data);
+        console.log(response.data)
+        console.log("hello")
+    } catch (error) {
+        res.status(500).send('Error fetching data');
+    }
+});
+
+app.listen(process.env.PORT ||3000, () => {
+    console.log('Server running on port 3000');
+});
